14ca563427e9ae64311a76e6e0561df158eb0764
ndakalajulius <julius.ndakala@student.moringaschool>
2022-10-24 11:46:08 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+    def countdown(count)
+        while count >0
+          puts "#{count} seconds!"
+          count-=1
+        end
+        "0 left. HAPPY NEW YEAR!"
+      end
+    
+      def countdown_with_sleep(count)
+        while count > 0
+          count-=1
+          sleep(5)
+        end
+          puts " HAPPY NEW YEAR!"
+      end
 
-def countdown
-end
+
